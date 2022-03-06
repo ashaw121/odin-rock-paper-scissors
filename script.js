c0686420c361fca4bfae8eb1dbf304d9af0e0215
Austin Shaw <ashaw121@gmail.com>
2022-03-06 08:22:59 -0500

    Finished RPS game

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ class Game {
   constructor() {
     this.userScore = 0;
     this.computerScore = 0;
+    this.scoreBoardText = '';
   }
 
   computerPlay() {
@@ -15,7 +16,7 @@ class Game {
   playRound(userPlay) {
     const computerPlay = this.computerPlay();
     let winner;
-    scoreBoard.innerHTML = `The user played ${userPlay}, the computer played ${computerPlay}`;
+    this.scoreBoardText = `The user played ${userPlay}, the computer played ${computerPlay}`;
     if (userPlay === computerPlay) return 'draw';
     if (userPlay === 'rock' && computerPlay === 'paper') winner = 'computer';
     if (userPlay === 'rock' && computerPlay === 'scissors') winner = 'user';
@@ -25,24 +26,24 @@ class Game {
     if (userPlay === 'scissors' && computerPlay === 'rock') winner = 'computer';
     if (userPlay === 'scissors' && computerPlay === 'paper') winner = 'user';
 
-    this.updateWinner(winner);
+    this.updateBoard(winner);
   }
 
   reportScore() {
-    scoreBoard.innerHTML += `The user has ${this.userScore} points and the computer has ${this.computerScore} points!`;
+    scoreBoard.innerHTML = `${this.scoreBoardText} The user has ${this.userScore} points and the computer has ${this.computerScore} points!`;
   }
 
-  updateWinner(winner) {
+  updateBoard(winner) {
     if (winner === 'draw') {
-      scoreBoard.innerHTML = 'This is a draw!';
+      this.scoreBoardText += `. This is a draw!`;
     }
     if (winner === 'computer') {
       this.computerScore++;
-      scoreBoard.innerHTML = 'The computer won this round!';
+      this.scoreBoardText += `. The computer won this round!`;
     }
     if (winner === 'user') {
       this.userScore++;
-      scoreBoard.innerHTML = 'The user won this round!';
+      this.scoreBoardText += '. The user won this round!';
     }
     this.reportScore();
 
@@ -50,6 +51,12 @@ class Game {
     //   console.log('The user wins the match!');
     // if (this.computerScore > this.userScore)
     //   console.log('The computer wins the match!');
+  }
+
+  resetGame() {
+    this.userScore = 0;
+    this.computerScore = 0;
+    scoreBoard.innerHTML = 'New Game!';
   }
 }
 
@@ -61,7 +68,9 @@ const btnPaper = document.querySelector('.btn__paper');
 const btnScissors = document.querySelector('.btn__scissors');
 const scoreBoard = document.querySelector('.scoreboard');
 
-btnNewGame.addEventListener('click', function () {});
+btnNewGame.addEventListener('click', function () {
+  game.resetGame();
+});
 btnRock.addEventListener('click', function () {
   game.playRound('rock');
 });
